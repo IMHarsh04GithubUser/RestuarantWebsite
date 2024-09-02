b3392c9acfca83d077fe68dc483af4f1d2ffab49
IMHarsh04GithubUser <141219591+IMHarsh04GithubUser@users.noreply.github.com>
2024-09-02 19:38:37 +0530

    Update index.js

--- RestBackend/index.js
+++ RestBackend/index.js
@@ -21,6 +21,7 @@ app.use(cors(
   origin: 'https://gourmatgarden-restaurant.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials:true,
   }
 )); 
 
